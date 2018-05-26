98fbda3e2e157bbdad5aaa2692a431f5a696c3b0
ZhangJin1988 <315552364@qq.com>
2018-05-26 11:19:35 +0800

    first scala

---
+++ src/com/scala/day1/HelloScala.scala
@@ -0,0 +1,11 @@
+package com.scala.day1
+
+/**
+  * desc:第一个scala程序 hello scala
+  */
+object HelloScala {
+  def main(args: Array[String]): Unit = {
+    println("hello scala")
+  }
+
+}
---
+++ src/com/scala/test/helloworld.scala
@@ -0,0 +1,6 @@
+package com.scala.test
+
+class helloworld {
+
+
+}
